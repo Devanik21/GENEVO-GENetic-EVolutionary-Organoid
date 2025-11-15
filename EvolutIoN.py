7af3af1124223a452ea99569b50d43d45cdef2ac
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-11-15 15:26:40 +0530

    Update EvolutIoN.py

--- EvolutIoN.py
+++ EvolutIoN.py
@@ -7478,6 +7478,7 @@ def main():
                         with code_col2:
                             st.subheader("TensorFlow / Keras Code")
                             tensorflow_code = generate_tensorflow_code(individual)
+                            st.code(tensorflow_code, language='python')
         
         else:
             st.info("The evolution has not been run or loaded successfully. Population data is missing for the analysis section.")
