c6e5d64c9bbce5474d1e41b3fe721fb2904b57fa
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-11-15 12:42:12 +0530

    Update EvolutIoN.py

--- EvolutIoN.py
+++ EvolutIoN.py
@@ -3446,7 +3446,8 @@ def main():
 
                        
                         if not st.session_state.current_population or not st.session_state.history:
-                        st.error("LOAD WARNING: The loaded checkpoint is incomplete. It's missing 'final_population_genotypes' or 'history' data. Analysis will fail. This file may be from an older script version.")
+                           st.error("LOAD WARNING: The loaded checkpoint is incomplete. It's missing 'final_population_genotypes' or 'history' data. Analysis will fail. This file may be from an older script version.")
+                        
 
                         # Force settings to update in the session state before rerun
                         if settings_table.get(doc_id=1): settings_table.update(st.session_state.settings, doc_ids=[1])
